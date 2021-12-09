8431d454ece71ddc2f3fd56befee96746a6e6e3e
Subash KC <subash.kc@tlgcohort.com>
2021-12-09 07:57:11 -0800

    Created Images and Give a Animation, Work with button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
-import Body from './components/Body'
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-require('dotenv').config()
-
+import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
+require("dotenv").config();
 
 function App() {
+  const title = `COVID-19 Tracker`;
   return (
     <div className="App">
-      <Header title={"COVID19 Tracker"} />
+      <Header title={title} />
       <Body />
-
     </div>
   );
 }
