826ca50fdd749d971efe67291cadbf861362e63d
zoeji569 <zoej569@gmail.com>
2021-12-10 15:01:46 -0800

    added US Map page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import About from "./About";
+import ContactUs from ".ContactUs";
+import Info from "./info";
+
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
 require("dotenv").config();
 
 function App() {
@@ -8,10 +16,26 @@ function App() {
   return (
     
     <div className="App">
+    
       <Header title={title} />
       
-      <Body />
+      
+      <Router>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contactus" element={<ContactUs />}/>
+          <Route path="/info" element={<Info />}/>
+        
+          <Route path = "/" element={<Body /> }/>
+      </Routes>
+      </Router>
+     
+      
+     
     </div>
+   
+    
+    
   );
 }
 
