d6e378f012c0fd6e529633e01ac6e7a7527791d1
Subash KC <subash.kc@tlgcohort.com>
2021-12-14 07:58:59 -0800

    Styling, Fixed Info page, Add Animation Images and Banner

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,18 +2,25 @@ import React from "react";
 import "../App.css";
 import Header from "../components/Header";
 
-
 function About() {
-
   return (
-
-    <div id="about_page" >
+    <div id="about_page" class="App">
       <Header />
       <h1>About this application</h1>
-      <p>Hello! Welcome to Covid-19 Tracker! The application is develped by Quentin, Subash and Zoe. This Covid-19 Tracker application built using React, uses data from CovidActNow as the data source.
-        The app displays key metrics around the growth of coronavirus, including the total number of population State and total number of population Country, new cases, new deaths, CDC Transmission Level, Risk Levels, and Test Positivity Ratio.
-        It displays data for  the US, including all the states and territories and worldwide stats.
-        Also displays chart in terms of Risk Level, Infection Rate, Compare death ration and Infection  </p><br />
+      <div className="about-text">
+      <p className="about-content">
+        Hello! Welcome to Covid-19 Tracker! The application is develped by <strong>Quentin</strong>, <strong>Subash</strong>, and <strong>Zoe</strong>. This Covid-19 Tracker application built using
+        React, uses data from CovidActNow as the data source. The app displays
+        key metrics around the growth of coronavirus, including the total number
+        of population State and total number of Population, Country, new cases,
+        New Deaths, CDC Transmission Level, Risk Levels, and Test Positivity
+        Ratio. It displays data for the US, including all the states and
+        territories and worldwide stats. Also displays chart in terms of Risk
+        Level, Infection Rate, Compare death ration and Infection{" "}
+      </p>
+      </div>
+      
+      <br />
     </div>
   );
 }
